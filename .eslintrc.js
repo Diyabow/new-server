c3583bd2dbe1486485a7ac2bff44ba242f1da1e7
ManvikaDivya <divyanm.22mca@jyotinivas.org>
2024-06-11 19:01:41 +0530

    some massage

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-var-requires': 0,
+      'prettier/prettier': [
+        'error',
+        { singleQuote: true },
+        { doubleQuote: true },
+      ],
+    },
+  };
+  
+  
